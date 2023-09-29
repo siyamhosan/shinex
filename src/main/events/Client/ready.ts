9e1e69b91a0cf8e476555d91f4a9f458b2c66965
Siyam Hosan <79569741+siyamhosan@users.noreply.github.com>
2023-09-29 16:31:38 +0600

    Initial commit

---
+++ src/main/events/Client/ready.ts
@@ -0,0 +1,17 @@
+import { Event } from 'dtscommands'
+import client from '../../../index.js'
+import chalk from 'chalk'
+
+export class ReadyEvent extends Event<'ready'> {
+  constructor () {
+    super({
+      name: 'ready',
+      once: true
+    })
+  }
+
+  run () {
+    console.info(`Client logged in as "${client.user?.tag}"`, chalk.bold('cli'))
+    // bot.application?.commands.set(bot.commands.array)
+  }
+}
