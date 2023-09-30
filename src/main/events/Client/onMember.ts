9c58c54663bc80a96d248579738adf648e843b6b
Siyam Hosan <siyam.hosan@yahoo.com>
2023-09-30 21:05:13 +0600

    added some

--- src/main/events/Client/onMember.ts
+++ src/main/events/Client/onMember.ts
@@ -14,12 +14,14 @@ export class NewMember extends Event<'guildMemberAdd'> {
     const guild = member.guild
     if (guild.id !== '1157258354821971998') return
 
+    await member.roles.add('1157693300074086423')
+
     await UpdateProfile(member.id, {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       badges: {
         push: 'MEMBER'
       }
-    })
+    }).catch(console.error)
   }
 }
--- src/utils/profile.ts
+++ src/utils/profile.ts
@@ -2,7 +2,12 @@ import { Badges } from '@prisma/client'
 
 export const BadgeEmojis: Record<Badges, string> = {
   EARLYSUPPORTER: '<:Early_ShineX:1157673614611583016>',
-  MEMBER: ''
+  MEMBER: '',
+  // <:shinex_admin:1157681259938594866> <:shinex_staff:1157677399778021437> <:report_shinex_staff:1157677327019425892> <:appeal_shinex_staff:1157677247914848267>
+  APPEAL_STAFF: '<:appeal_shinex_staff:1157677247914848267>',
+  REPORT_STAFF: '<:report_shinex_staff:1157677327019425892>',
+  SHINEX_ADMIN: '<:shinex_admin:1157681259938594866>',
+  SHINEX_STAFF: '<:shinex_staff:1157677399778021437>'
 }
 
 export function GetBadges (badges: Badges[]) {
